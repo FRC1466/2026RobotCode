4523090c89c704a8bf896046f5bcee54704350c9
Anima3213 <109627078+GrumpyBud@users.noreply.github.com>
2026-03-19 11:00:45 -0400

    Update at 'Thu Mar 19 11:00:45 EDT 2026'

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -41,6 +41,7 @@ import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -141,6 +142,8 @@ public class Drive extends SubsystemBase {
     modules[1] = new Module(frModuleIO, 1, TunerConstants.FrontRight);
     modules[2] = new Module(blModuleIO, 2, TunerConstants.BackLeft);
     modules[3] = new Module(brModuleIO, 3, TunerConstants.BackRight);
+
+    SmartDashboard.putData("Current Pose", poseField);
 
     setpointGenerator =
         new SwerveSetpointGenerator(
