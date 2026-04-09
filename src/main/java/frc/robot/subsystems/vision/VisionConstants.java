568043d8a52fd3aad65f3784a39f8cf2e6b9fd66
Anima3213 <109627078+GrumpyBud@users.noreply.github.com>
2026-04-09 13:37:33 -0400

    Update at 'Thu Apr 09 13:37:33 EDT 2026'

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -27,11 +27,11 @@ public class VisionConstants {
           Units.inchesToMeters(3.977574),
           Units.inchesToMeters(-9.194390),
           Units.inchesToMeters(20.573830),
-          new Rotation3d(0.0, Units.degreesToRadians(-70), 0.0));
+          new Rotation3d(0.0, Units.degreesToRadians(-20), 0.0));
   public static Transform3d robotToCamera1 =
       new Transform3d(
           Units.inchesToMeters(.842),
-          Units.inchesToMeters(9.189),
+          Units.inchesToMeters(-9.189),
           Units.inchesToMeters(14.067),
           new Rotation3d(Math.PI, Units.degreesToRadians(70), Math.PI));
 
